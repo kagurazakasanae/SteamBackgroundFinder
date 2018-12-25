5d0d6b60ab150791204623d1bb58a9fdd20766a7
神楽坂早苗 <kochiya@sanae.cc>
2018-12-25 22:02:28 +0800

    Update form.js

--- static/js/form.js
+++ static/js/form.js
@@ -1,7 +1,7 @@
 /*
 
 	Steam Background Finder
-    Copyright (C) 2018 Xxmarijnw
+    Copyright (C) 2018 Xxmarijnw & Modified by KagurazakaSanae
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -32,7 +32,7 @@ $(document).ready(function(){
 		var formData = $("form").serialize();
 		// Send AJAX request
 		$.ajax({
-			url: 'handler',
+			url: 'handler.php',
 			type: 'POST',
 
 			// Form data
