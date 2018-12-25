681f385c47a81f818f2619186781fa95e026a9d1
神楽坂早苗 <kochiya@sanae.cc>
2018-12-25 21:43:57 +0800

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,7 +3,7 @@
 /*
 
 	Steam Background Finder
-    Copyright (C) 2018 Xxmarijnw
+    Copyright (C) 2018 Xxmarijnw & Modified by KagurazakaSanae
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -20,81 +20,133 @@
 	
 */
 
-if(!isset($_POST)) 
-{
-	exit();
-}
-else 
-{ 
-	$link = basename($_POST['url']);
-}
+@$link = $_POST['url'];
 
-if(empty($_POST['url']))
-{
+if(trim($link) == '' || !filter_var($link, FILTER_VALIDATE_URL)){
 	echo 'Oops! You forgot to enter something.';
-	exit();
+	die;
 }
 
-$link = ((strlen($link) === 17) && (is_numeric($link)) && (substr( $link, 0, 7 ) === '7656119' )) ? 'https://steamcommunity.com/profiles/'.$link : 'https://steamcommunity.com/id/'.$link;
+if(!stristr($link, 'steamcommunity.com')){
+	echo 'Please be sure you are providing an valid Steam community profile URL.';
+	die;
+}
 
-$profile = file_get_contents($link);
+$profile = urllib('GET', $link);
 
 preg_match('/<div class="error_ctn">/', $profile, $profile_exists);
-if($profile_exists)
-{
+if($profile_exists){
 	echo 'Oops! This profile was not found.';
 	exit();
 }
 	
 preg_match('/<div class="profile_private_info">/', $profile, $profile_private);
-if($profile_private)
-{
+if($profile_private){
 	echo 'Oops! This profile is set to private.';
 	exit();
 }
-	
-preg_match('/(?<=background-image: url\(https:\/\/)(.+)(?=\);")/', $profile, $background);
-if(empty($background))
-{
+
+$background = getUserProfileBGUrl($link);
+if($background == -1){
 	echo 'Oops! This profile has no background.';
 	exit();
 }
 
-$background          = 'https://'.$background[0];
+$bg_data = getBGMarketUrl($link);
+if($bg_data == -2 or $bg_data == -1 or $bg_data == -4){
+	echo 'Our server failed to access to Steam\'s server, please try again later';
+	die;
+}
+if($bg_data == -3 || $bg_data == -5){
+	echo 'We can not fetch out any results that match this background, please search it manually';
+	die;
+}
 
-$segments            = explode('/', parse_url($background, PHP_URL_PATH));
-$appid               = $segments[5];
+$appid = $bg_data['appid'];
 
-$app_details         = json_decode(file_get_contents('https://store.steampowered.com/api/appdetails?appids='.$appid), true);
-$app_name            = $app_details[$appid]['data']['name'];
-$app_name            = ($app_name !== null) ? 'This background is from '.$app_name : '';
+$app_details = json_decode(urllib('GET', 'https://store.steampowered.com/api/appdetails?appids='.$appid), true);
+@$app_name = $app_details[$appid]['data']['name'];
+$app_name = ($app_name !== null) ? 'This background is from '.$app_name : '';
 
 $steam_card_exchange = 'https://www.steamcardexchange.net/index.php?gamepage-appid-'.$appid;
-$steamdesign         = 'https://steam.design/#'.$background;
-$market              = 'https://steamcommunity.com/market/search?q=&category_753_item_class[]=tag_item_class_3&appid=753&category_753_Game[]=tag_app_'.$appid;
-
-$results             = json_decode(file_get_contents('https://www.googleapis.com/customsearch/v1?key=&cx=006499585084668756177:3iopj7jzexa&siteSearch=https://steamcommunity.com&q='.$background), true);
-$count               = $results['queries']['request'][0]['count'];
+$steamdesign = 'https://steam.design/#'.$background;
 			
 $price = '';
 
-if(!(array_key_exists('error', $results)) || ($count != '0')) 
-{
-	$check = $results['items'][0]['formattedUrl'];
-	
-	if(strpos($check, $appid))
-	{
-		$market           = $results['items'][0]['link'];
-		$segments         = explode('/', parse_url($market, PHP_URL_PATH));
-		$market_hash_name = $segments[4];
-		$price_overview   = json_decode(file_get_contents("https://steamcommunity.com/market/priceoverview/?appid=753&currency=1&market_hash_name=".$market_hash_name), true);
-		if (($price_overview['success'] === true) && ($price_overview['lowest_price'] !== null))
-		{
-			' ('.$price_overview['lowest_price'].')';
-		}
-	}
+$market = $bg_data['url'];
+$market_hash_name = $bg_data['hash_name'];
+@$price_overview   = json_decode(urllib('GET', "https://steamcommunity.com/market/priceoverview/?appid=753&currency=1&market_hash_name=".$market_hash_name), true);
+if(($price_overview['success'] === true) && ($price_overview['lowest_price'] !== null)){
+	' ('.$price_overview['lowest_price'].')';
 }
 
 echo json_encode(array('background'=>$background, 'steam_card_exchange'=>$steam_card_exchange, 'market'=>$market, 'app_name'=>$app_name, 'steamdesign'=>$steamdesign, 'price'=>$price));
+
+function getBGMarketUrl($bgurl){
+	$t = explode('/', $bgurl);
+	$appid = $t[array_search('items', $t) + 1];
+	$filename = $t[array_search('items', $t) + 2];
+	$market_search_contents = urllib('GET', 'https://steamcommunity.com/market/search?category_753_Game%5B%5D=tag_app_' . $appid . '&category_753_item_class%5B%5D=tag_item_class_3&appid=753');
+	if(!stristr($market_search_contents, 'Showing results for')){
+		return -2;	//content error
+	}
+	if(stristr($market_search_contents, 'There were no items matching your search')){
+		return -1;	//appid error
+	}
+	if(preg_match_all('/(<a class="market_listing_row_link" href=)(".*?")( id)(=)(".*?")(>)/is', $market_search_contents, $matches)){
+		$links = array();
+		foreach($matches[2] as $s){
+			$links[] = str_replace('"', '', $s);
+		}
+	}else{
+		return -3;	//no search results
+	}
+	foreach($links as $s){
+		$item_detail_page_contents = urllib('GET', $s);
+		if(!stristr($item_detail_page_contents, 'View Full Size')){
+			return -4;	//content error2
+		}
+		if(stristr($item_detail_page_contents, $filename)){
+			$title = str_replace('Steam Community Market :: Listings for ', '', explode('</title>', explode('<title>', $item_detail_page_contents)[1])[0]);
+			$hash_name = explode('/', $s)[array_search('listings', $t) + 2];
+			return array('name' => $title, 'url' => $s, 'appid' => $appid, 'hash_name' => $hash_name);
+		}
+	}
+	return -5;	//no matching results
+}
+
+function getUserProfileBGUrl($profile_url){
+	$profile_page_content = urllib('GET', $profile_url);
+	if(!stristr($profile_page_content, 'no_header profile_page has_profile_background')){
+		return -1;	//can not get background link
+	}
+	$link = explode('\' );">', explode('<div class="no_header profile_page has_profile_background " style="background-image: url( \'', $profile_page_content)[1])[0];
+	return $link;
+}
+
+function urllib($function, $url, $data = array(), $header = array()){
+	if(!in_array('User-Agent', $header)){
+		$header['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36';
+	}
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+	curl_setopt($curl, CURLOPT_HEADER, 0);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(isset($header['Cookie'])){
+		curl_setopt($curl, CURLOPT_COOKIE, $header['Cookie']);
+	}
+	if($function == 'POST'){
+		$data = http_build_query($data);
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	}
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+	$r = curl_exec($curl);
+	//var_dump(curl_error($curl));
+	//var_dump(curl_errno($curl));
+	curl_close($curl);
+	return $r;
+}
 
 ?>
